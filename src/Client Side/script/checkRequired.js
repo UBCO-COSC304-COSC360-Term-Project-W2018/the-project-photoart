adaa0c34cd6a6554e54ce2fb6dacac9e4940e693
Jan Reisler <agentmikster44@gmail.com>
2018-10-25 19:45:02 -0700

    Register JS

--- src/Client Side/script/checkRequired.js
+++ src/Client Side/script/checkRequired.js
@@ -1,12 +1,35 @@
-window.onload = function(){
-  var form = document.getElementById("mainForm");
-  form.addEventListener("submit", checkInput);
+document.addEventListener("click", checkTarget);
+
+function checkTarget(e){
+  var target = e.target;
+  if (target.id == "submit")
+    checkInput(e);
+}
+
 
   function checkInput(e){
+    //check if passwords match
+    var pwd1 = document.getElementById("pwd1");
+    var pwd2 = document.getElementById("pwd2");
+    if (pwd1.value !== pwd2.value)
+      alert("Passwords don't match");
+    if (pwd1.value.length <= 8)
+      alert("Password must be at least 8 characters long")
+
+    //check if email is valid
+    var email = document.getElementById("email");
+    if (email.value.indexOf("@") == -1)
+      alert("Please enter a valid email");
+
+    //check if all required fields are filled
     var req = document.getElementsByClassName("required");
     for(var i = 0; i < req.length; i++){
       if (req[i].classList.contains("markReq"))
-        e.preventDefault();
+        if (req[i].value.length <= 0)
+          e.preventDefault();
+        else {
+          e.target.classList.toggle("markReq");
+        }
       else{
         if (req[i].value.length <= 0){
           req[i].classList.toggle("markReq");
@@ -17,7 +40,7 @@ window.onload = function(){
     }
   }
 
+//If user inputs text, remove red warning
   function update(e){
-    e.target.classList.toggle("mark");
+    e.target.classList.toggle("markReq");
   }
-}
