e55873f4163f39cd224b6f13aa9b9a11f0116306
Jan Reisler <agentmikster44@gmail.com>
2018-11-28 15:03:49 -0800

    removed search.php and integrated picture info with search

--- src/server_side/credentials.php
+++ src/server_side/credentials.php
@@ -1,20 +1,11 @@
 <?php
-$host = "cosc304.ok.ubc.ca";
-$database = "group12";
-$user = "group12";
-$password = "group12";  //UNCOMMENT THIS BEFORE PUSHING!!!!
+// $host = "cosc304.ok.ubc.ca";
+// $database = "group12";
+// $user = "group12";
+// $password = "group12";  //UNCOMMENT THIS BEFORE PUSHING!!!!
 
-
-// $host = "localhost";
-// $database = "PhotoArt";
-// $user = "root";
-// $password = ""; //COMMENT THIS BEFORE PUSHING
-
-<<<<<<< HEAD
-/*$host = "localhost";
+$host = "localhost";
 $database = "PhotoArt";
 $user = "root";
-$password = "";*/ //COMMENT THIS BEFORE PUSHING
-=======
->>>>>>> 411a01ed2e88bf3de6e33e786875bc75a6a164c0
+$password = ""; //COMMENT THIS BEFORE PUSHING
  ?>
