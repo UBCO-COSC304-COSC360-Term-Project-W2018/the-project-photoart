a9cb7f5224a35cf3df1a259705ac814937487168
Jan Reisler <agentmikster44@gmail.com>
2018-11-28 12:16:24 -0800

    Update credentials.php

--- src/server_side/credentials.php
+++ src/server_side/credentials.php
@@ -1,6 +1,10 @@
 <?php
-$host = "cosc304.ok.ubc.ca";
-$database = "group12";
-$user = "group12";
-$password = "group12";
+// $host = "cosc304.ok.ubc.ca";
+// $database = "group12";
+// $user = "group12";
+// $password = "group12";
+$user = "root";
+$password = "";
+$database = "photoart";
+$host = "localhost";
  ?>
