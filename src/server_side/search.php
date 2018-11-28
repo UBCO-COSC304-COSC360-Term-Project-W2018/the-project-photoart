e48bdc47ff79ac94529e79c4b5fde9b5f10cfb59
Rohan <rohan.chauhan@hotmail.com>
2018-11-27 19:37:34 -0800

    searchbar functionality

--- src/server_side/search.php
+++ src/server_side/search.php
@@ -3,7 +3,7 @@
 require('../server_side/header.php');
 require('../server_side/connection.php');
   $search = $_GET["searchBar"];
-  $sql = ("SELECT title, imageLink FROM Product WHERE title LIKE '%'. $search .'%'");
+  $sql = ("SELECT title, imageLink FROM Product WHERE title LIKE '%". $search . "%'");
   $result = mysqli_query($con, $sql);
   if(mysqli_num_rows($result) > 0){
     while($row = mysqli_fetch_assoc($result)){
@@ -11,7 +11,6 @@ require('../server_side/connection.php');
       $imgLink = $row['imageLink'];
       echo $title;
       echo $imgLink;
-      $sql->close();
   }
 }
   else{
