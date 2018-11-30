410bc153204710600a6b115195e3b47a9edd5bf0
Jan Reisler <agentmikster44@gmail.com>
2018-11-30 08:37:28 -0800

    Made pages be displayed properly and better

--- src/client_side/PhotoArtExplore.php
+++ src/client_side/PhotoArtExplore.php
@@ -12,7 +12,6 @@
   <?php require('../server_side/header.php'); ?>
     <div id="mainBG" class="shadow">
       <h2>Explore - Click on a photo to view similar photos!</h2>
-      <div id="sidebyside">
         <div id="columnWrapper" class="shadow">
           <a href="PhotoArtAbstract.php"><img src="images/Abstract/Fire.jpg" alt="A vibrant fire" id="images"></a>
           <h3>Abstract -<span id="ital"> Explore the wild side</span></h3>
@@ -21,8 +20,6 @@
           <a href="PhotoArtLandscape.php"><img src="images/Landscape/slopes.jpg" alt="Cascading lush slopes" id="images"></a>
           <h3>Landscape -<span id="ital"> Wider angles makes your mouth dangle</span></h3>
         </div>
-        </div>
-      <div id="sidebyside2">
         <div id=columnWrapper class="shadow">
           <a href="PhotoArtNature.php"><img src="images/Nature/Creek.jpg" alt="A creek with clear water" id="images"></a>
           <h3>Nature -<span id="ital"> Beautiful escapes provided by mother Earth</h3>
@@ -30,8 +27,7 @@
         <div id="columnWrapper" class="shadow">
           <a href="PhotoArtDogs.php"><img src="images/Dogs/Thunder.jpg" alt"A very cute dog" id="images"></a>
           <h3>Dogs -<span id="ital"> Need we say more?</h3>
-        </div>
-      </div>
+        </div></div>
     <footer>
       <p>&copy; 2018 PhotoArt All Rights Reserved | Website created for COSC 360/304</p>
     </footer>
