5eafffd3d24f636ad39e6dd643bb7d06032ce14a
Rohan <rohan.chauhan@hotmail.com>
2018-11-29 23:09:19 -0800

    added descriptions to images

--- src/client_side/PhotoArtNature.php
+++ src/client_side/PhotoArtNature.php
@@ -8,18 +8,19 @@ require('../server_side/connection.php');?>
 <div id='columnWrapper' class='shadow'>
 <h1>Nature - <span id="ital"> Beautiful escapes provided by mother Earth</span></h1>
 <?php
-$sql = ("SELECT title, imageLink, upc, category FROM Product WHERE category='Nature'");
+$sql = ("SELECT title, imageLink, upc, category, description FROM Product WHERE category='Nature'");
 $result = mysqli_query($con, $sql);
 if(mysqli_num_rows($result) > 0){
   while($row = mysqli_fetch_assoc($result)){
     $title = $row['title'];
     $imgLink = $row['imageLink'];
     $imgUPC = $row['upc'];
+    $description = $row['description'];
 echo"<div id=resultSection class='shadow'>
 <p id='titles'>".$title."</p>
 <br>
   <span><a href='PhotoArtPictureInfo.php?upc=".$imgUPC."'><img src=".$imgLink." alt='Photo of the week' id='images'></a></span> <!-- this links to the photoart picture info page -->
-
+  <p>".$description. "</p>
 </div>";
 }
 }
