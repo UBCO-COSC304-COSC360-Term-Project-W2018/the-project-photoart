5c7eb4333da9b2aa8acd3b06933bc50dbccc336d
matieunibre <matt.carrier183@gmail.com>
2018-11-30 11:23:21 -0800

    stuck

--- src/server_side/updatePaymentInfo.php
+++ src/server_side/updatePaymentInfo.php
@@ -6,31 +6,32 @@ if(isset($_SESSION['username'])){
 if($sql=$con->prepare("SELECT * from PaymentInfo  where username =?")){
   $sql->bind_param('s', $user);
   $sql->execute();
-  $firstN=$_POST['firstName'];
-  $lastName=$_POST['lastName'];
-  $address=$_POST['address'];
-  $country =$_POST['country'];
-  $province = $_POST['Province'];
-  $postal=$_POST['postal'];
-  $email=$_POST['email'];
-  $name=$_POST['Payment'];
-  $carNum=$_POST['CardNumber'];
-  $cardDate=$_POST['CardDate'];
-  $cardCSV=$_POST['CardCSV'];
-  $fullName=$firstN." ".$lastName;
+   $firstN=$_POST['firstName'] ."<br>";
+   $lastN=$_POST['lastName'] . "<br>";
+echo $address=$_POST['address'] ."<br>";
+echo $country =$_POST['country']."<br>";
+echo $province = $_POST['Province']."<br>";
+echo $postal=$_POST['postal']."<br>";
+echo $email=$_POST['email']."<br>";
+echo $name=$_POST['Payment']."<br>";
+echo $cardNum=$_POST['CardNumber']."<br>";
+echo $cardDate=$_POST['CardDate']."<br>";
+echo $cardCSV=$_POST['CardCSV']."<br>";
+echo $fullName=$firstN." ".$lastN;
   if($sql->num_rows){
 
 
   if($stmt=$con->prepare("UPDATE PaymentInfo set cardNum=?,username=?,nameOnCard=?,expDate=?,CSV=?,billingAddress=?,country=?,province=?,city=?,postalCode=?")){
-    //$stmt->bind_param('iss');
+    //$stmt->bind_param('i',$);
     }else{
 
         }
 }else{
 echo "here";
+
   //nothing found new payment info
-    $stmt1=$con->prepare("INSERT INTO PaymentInfo (cardNum,username,nameOnCard,expDate,CSV,billingAddress,country,province,city,postalCode)values(?,?,?,?,?,?,?,?,?,?)");
-    $stmt1->bind_param('isssisssss',$cardNum,$user,$fullName,$cardDate,$cardCSV,$address,$country,$province,$city,$postal );
+    $stmt1=$con->prepare("INSERT INTO PaymentInfo (cardNum,username,nameOnCard,expDate,CSV,billingAddress,country,province,city,postalCode) values (?,?,?,?,?,?,?,?,?,?)");
+    $stmt1->bind_param('ssssssssss',$cardNum,$user,$fullName,$cardDate,$cardCSV,$address,$country,$province,$city,$postal );
     $stmt1->execute();
     echo "it worked";
 
