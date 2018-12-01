83aed1ef619dd54ac5c914f00d771f0c101922bd
Rohan <rohan.chauhan@hotmail.com>
2018-11-30 19:01:12 -0800

    need to push, so gotta commit

--- src/server_side/credentials.php
+++ src/server_side/credentials.php
@@ -4,11 +4,6 @@ $database = "group12";
 $user = "group12";
 $password = "group12";  //UNCOMMENT THIS BEFORE PUSHING!!!!
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> d970780b057e8deadcac7d8b5be83811fe9e1e98
 // $host = "localhost";
 // $database = "PhotoArt";
 // $user = "root";
