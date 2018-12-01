3c61aafb6aa3656b3204d6a7f38409c412e316a6
Rohan <rohan.chauhan@hotmail.com>
2018-11-30 19:58:53 -0800

    minor fixes

--- src/server_side/credentials.php
+++ src/server_side/credentials.php
@@ -2,13 +2,8 @@
 $host = "cosc304.ok.ubc.ca";
 $database = "group12";
 $user = "group12";
-$password = "group12";  //UNCOMMENT THIS BEFORE PUSHING!!!!
+$password = "group12";  //UNCOMMENT THIS BEFORE PUSHING
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 95d88766f92ae041795a8ce2eb166d091960aea6
 // $host = "localhost";
 // $database = "PhotoArt";
 // $user = "root";
