6ba9a4fd6e521f8ba7a638600ecf9728a71190dc
Rohan <rohan.chauhan@hotmail.com>
2018-11-30 16:42:18 -0800

    prepared statement

--- src/server_side/credentials.php
+++ src/server_side/credentials.php
@@ -1,20 +1,11 @@
 <?php
-// $host = "cosc304.ok.ubc.ca";
-// $database = "group12";
-// $user = "group12";
-// $password = "group12";  //UNCOMMENT THIS BEFORE PUSHING!!!!
+$host = "cosc304.ok.ubc.ca";
+$database = "group12";
+$user = "group12";
+$password = "group12";  //UNCOMMENT THIS BEFORE PUSHING!!!!
 
-
-
-<<<<<<< HEAD
 // $host = "localhost";
 // $database = "PhotoArt";
 // $user = "root";
 // $password = "";  //COMMENT THIS BEFORE PUSHING
-=======
-$host = "localhost";
-$database = "PhotoArt";
-$user = "root";
-$password = ""; //COMMENT THIS BEFORE PUSHING
->>>>>>> 2ab8d11523d232d8c89899c2953b701d1f9e3624
 ?>
